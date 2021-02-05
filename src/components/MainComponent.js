96e1239165822afa08e5242f927eba1bb94ae790
dvdlm914 <dvdlm914@gmail.com>
2021-02-05 11:55:39 -0500

    footer added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from "./Header";
+import Footer from "./Footer"
 
 class Main extends Component {
     constructor(props) {
@@ -23,6 +24,7 @@ class Main extends Component {
              <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+            <Footer />
             </div>
         );
     }
